c0add65f35ba549f09f30259c71306465cdc439f
Kneelawk <kneelawk@gmail.com>
2023-09-16 00:11:54 -0700

    Add Modrinth Publishing

---
+++ changelogs/changelog-v0.1.7.md
@@ -0,0 +1,4 @@
+Changes:
+
+* Started on Modrinth publishing.
+* This modpack is still incomplete and lacking some important mods that have not been released for 1.20 yet.
---
+++ ci/publish-modrinth.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_NAME="Innovation Timespace 7"
+PROJECT_ID="bchAhFQi"
+
+TAG_VERSION=${TAG_NAME:1}
+
+CHANGELOG="$(cat "changelogs/changelog-$TAG_NAME.md" | jq -Rsa .)"
+
+read -r -d '' CREATE_VERSION_JSON <<EOF
+{
+    "name": "$PROJECT_NAME $TAG_NAME",
+    "version_number": "$TAG_VERSION",
+    "changelog": "$CHANGELOG",
+    "dependencies": [],
+    "game_versions": [
+        "1.20.1"
+    ],
+    "version_type": "alpha",
+    "loaders": [
+        "quilt"
+    ],
+    "featured": false,
+    "status": "listed",
+    "requested_status": "listed",
+    "project_id": "$PROJECT_ID",
+    "file_parts": [
+        "file"
+    ],
+    "primary_file": "file"
+}
+EOF
+
+echo "Sending Request:"
+echo "$CREATE_VERSION_JSON"
+
+FILENAME="$PROJECT_NAME-$TAG_VERSION.mrpack"
+
+echo "Getting Response"
+curl -H "Authorization: $MODRINTH_TOKEN" -F "data=$CREATE_VERSION_JSON" -F file=@"$FILENAME" 'https://api.modrinth.com/v2/version'
+
